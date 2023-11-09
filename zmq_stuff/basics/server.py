824bcf7f3719467ae23a8fe36602adabb79fc11c
bsaranga <bsaranga@gmail.com>
2023-11-09 22:31:16 +0530

    zmq server

--- zmq_stuff/basics/server.py
+++ zmq_stuff/basics/server.py
@@ -1,16 +1,19 @@
+import os
 import zmq
 import signal
 
 signal.signal(signal.SIGINT, 0)
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://*:5555")
-
+print(f"PID: {os.getpid()}")
 print("Listening on port 5555")
 
-while True:
-    #  Wait for next request from client
-    message = socket.recv_string()
-    print(f"Received request: {message}")
-    socket.send(f"Ack: {message}")
+with zmq.Context() as context:
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+
+    while True:
+        #  Wait for next request from client
+        message = socket.recv_string()
+        print(f"Received request: {message}")
+        socket.send_string(f"Ack: {message}")
