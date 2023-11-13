1d12b0d9039b03825eaad1ff307f5b8f6ba42e30
bsaranga <bsaranga@gmail.com>
2023-11-13 21:57:21 +0530

    async stuff

---
+++ asyncio_stuff/asynchelloworld.py
@@ -0,0 +1,12 @@
+import asyncio
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
+async def main():
+    await asyncio.gather(count(), count(), count())
+
+if __name__ == "__main__":
+    asyncio.run(main())
