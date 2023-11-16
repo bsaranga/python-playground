626f5e0384bee1d71480d41295e92bac438f6878
Buwaneka Saranga <bsaranga@gmail.com>
2023-11-16 21:11:28 +0530

    generators++

--- generators.py
+++ generators.py
@@ -4,7 +4,24 @@ def naturalNumberGen(n):
         yield num
         num += 1
 
-for i in naturalNumberGen(10):
+""" for i in naturalNumberGen(10):
     print(i)
 
-print(sum(naturalNumberGen(1000)))
+print(sum(naturalNumberGen(1000))) """
+
+
+def csvSplitter(text: str):
+    i = 0
+    buffer = ''
+    while i < len(text):
+        if (text[i] == ','):
+            result = buffer
+            buffer = ''
+            yield result
+        else:
+            buffer += text[i]
+        i += 1
+
+
+for word in csvSplitter('hello,world,monty,python'):
+    print(word)
