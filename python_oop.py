57fc2f126cf65e4cb348d6e518d97f6130e2df5d
Buwaneka Saranga <bsaranga@gmail.com>
2024-04-24 19:26:53 +0530

    Add Developer and Manager classes with attributes and methods

--- python_oop.py
+++ python_oop.py
@@ -24,3 +24,30 @@ class Employee:
         if day.weekday() == 5 or day.weekday() == 6:
             return False
         return True
+    
+class Developer(Employee):
+    raise_amount = 1.10
+
+    def __init__(self, firstName, lastName, pay, prog_lang) -> None:
+        super().__init__(firstName, lastName, pay)
+        self.prog_lang = prog_lang
+
+class Manager(Employee):
+    def __init__(self, firstName, lastName, pay, employees=None) -> None:
+        super().__init__(firstName, lastName, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_employee(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    
+    def remove_employee(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+    
+    def print_employees(self):
+        for emp in self.employees:
+            print("-->", emp.fullName())
